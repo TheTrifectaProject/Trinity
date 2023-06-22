d9f15496de683228cc368247682aba5f64b6e813
Ty Hosn <Abolsonm3@gmail.com>
2023-06-21 19:35:27 -0700

    setup for base

--- Client/Reacts/my-app/src/components/Nav.js
+++ Client/Reacts/my-app/src/components/Nav.js
@@ -1,10 +1,10 @@
 import react from 'react'
-
+import styled from "styled-components"
 import {useState} from 'react'
 
-//looks good im trying to see where to put the styled componenets TRUE we can do either or but i do want to try the oter one  
+//looks good im trying to see where to put the styled componenets TRUE we can do either or but i do want to try the oter one
 //lets use styled components?  yup bet
-//LOL BRO      i was literally thinking about that 
+//LOL BRO      i was literally thinking about that
 //FIX UR THING WHERE THE CODE GETS ALL MESSED UP LOL :P
 //IM GOING TO IMPORT THIS FILE INTO THE APP.js
 //i did two child divs inside the "nav--bar" so we can flex them, logo on the left side, and the search bar on the right hand side
@@ -17,15 +17,15 @@ color: white;
 `
 
 const Nav = () => {
-    return ( 
+    return (
         <div className = "nav--bar" >
             <div className = "nav-logo" >
-                <img> </img>
+                <img/>
             </div>
 
             <div className = "nav--search" >
-                <input placeholder = 'search' > </input>
-                <SearchButton>🔎</SearchButton> 
+                <input placeholder = 'search' />
+                <SearchButton>🔎</SearchButton>
             </div>
 
         </div>
@@ -40,4 +40,4 @@ const StyledNav = styled(Nav)`
 `;
 
 
-export default StyledNav 
+export default StyledNav
